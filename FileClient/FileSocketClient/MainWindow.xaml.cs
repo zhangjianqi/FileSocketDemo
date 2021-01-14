8add2e29c8567b7691e7f84d38a9776cb86d5c8b
若汝棋茗 <76547834+RRQM@users.noreply.github.com>
2021-01-14 13:24:53 +0800

    文件转移
    文件转移

---
+++ FileClient/FileSocketClient/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using FileSocketClient.ViewModels;
+using RRQMSkin.Windows;
+
+namespace FileSocketClient
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : RRQMWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Window = this;
+        }
+
+        public static MainWindow Window { get; set; }
+    }
+}
---
+++ FileService/FileSocketService/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using FileSocketService.ViewModels;
+using RRQMSkin.Windows;
+using System.Windows;
+
+namespace FileSocketService
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow :RRQMWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Window = this;
+            RRQMMVVM.SimpleIoc.Default.Register(this,new MainViewModel());
+        }
+        public static Window Window;
+    }
+}
